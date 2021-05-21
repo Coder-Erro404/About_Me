5c514276605549bd147cf2ed1646e069a700d489
kunalKCoder <kkhansare@gmail.com>
2021-05-21 19:22:54 +0530

    make it better

--- resume_app/static/pred_app/js/main.js
+++ resume_app/static/pred_app/js/main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["kanhaiya khansare", "Msc-IT-part-2 stundent"];
+const textArray = ["Kanhaiya Khansare", "Web Developer","Software Tester"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
